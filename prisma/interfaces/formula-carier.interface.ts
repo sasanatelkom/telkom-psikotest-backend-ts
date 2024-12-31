b9727c00d2105d6a7e3a1d07d6272a225ea6a1c4
fikiaprian23 <fikiaprian23@gmail.com>
2024-12-31 11:51:46 +0700

    add data formula carier

---
+++ prisma/interfaces/formula-carier.interface.ts
@@ -0,0 +1,12 @@
+import { TypeOrientation } from "@prisma/client";
+
+export interface IFormulaCarier {
+    orientation: TypeOrientation;
+    cluster1: string;
+    cluster2: string;
+    cluster3: string;
+    codeSDS: string;
+    code1: string;
+    code2: string;
+    program: string;
+}
