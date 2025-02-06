72267d532883b92abcd3013aaa38f62eb2dcdd96
fikiaprian23 <fikiaprian23@gmail.com>
2025-02-06 09:46:24 +0700

    feat: get all AllParticipant without paginte

--- src/participant/participant.controller.ts
+++ src/participant/participant.controller.ts
@@ -1,15 +1,15 @@
 import {
-    Body,
-    Controller,
-    Get,
-    Put,
-    HttpStatus,
-    Param,
-    Post,
-    Delete,
-    Res,
-    UseGuards,
-    Query,
+  Body,
+  Controller,
+  Get,
+  Put,
+  HttpStatus,
+  Param,
+  Post,
+  Delete,
+  Res,
+  UseGuards,
+  Query,
 } from '@nestjs/common';
 import { ParticipantService } from './participant.service';
 import { HttpHelper } from '../helpers/http.helper';
@@ -23,63 +23,67 @@ import { SearchPaginationDto } from '../utils/dto/pagination';
 
 @Controller('participant')
 export class ParticipantController {
-    constructor(
-        private readonly participantService: ParticipantService,
-        private readonly httpHelper: HttpHelper,
-        private readonly validation: Validation,
-    ) { }
+  constructor(
+    private readonly participantService: ParticipantService,
+    private readonly httpHelper: HttpHelper,
+    private readonly validation: Validation,
+  ) {}
 
-    @Get()
-    async getAllParticipants(
-        @Query() dto: SearchPaginationDto,
-        @Res() res: Response,
-    ) {
-        try {
-            const result = await this.participantService.getManyPaginate(dto);
-            return this.httpHelper.formatResponse(res, HttpStatus.OK, result);
-        } catch (error) {
-            this.validation.errorHandler(res, error);
-        }
+  @Get()
+  async getAllParticipants(@Res() res: Response) {
+    try {
+      const result = await this.participantService.getAllParticipants();
+      return this.httpHelper.formatResponse(res, HttpStatus.OK, result);
+    } catch (error) {
+      this.validation.errorHandler(res, error);
     }
+  }
 
-    @Get(':id')
-    async getParticipantById(
-        @Param('id') id: string,
-        @Res() res: Response,
-    ) {
-        try {
-            const result = await this.participantService.getParticipantById(id);
-            return this.httpHelper.formatResponse(res, HttpStatus.OK, result);
-        } catch (error) {
-            this.validation.errorHandler(res, error);
-        }
+  @Get('paginate')
+  async getAllParticipantsPaginate(
+    @Query() dto: SearchPaginationDto,
+    @Res() res: Response,
+  ) {
+    try {
+      const result = await this.participantService.getManyPaginate(dto);
+      return this.httpHelper.formatResponse(res, HttpStatus.OK, result);
+    } catch (error) {
+      this.validation.errorHandler(res, error);
     }
+  }
 
-    @Post()
-    async createParticipant(
-        @Body() dto: CreateParticipantDto,
-        @Res() res: Response,
-    ) {
-        try {
-            const result = await this.participantService.createParticipant(dto);
-            return this.httpHelper.formatResponse(res, HttpStatus.CREATED, result);
-        } catch (error) {
-            this.validation.errorHandler(res, error);
-        }
+  @Get(':id')
+  async getParticipantById(@Param('id') id: string, @Res() res: Response) {
+    try {
+      const result = await this.participantService.getParticipantById(id);
+      return this.httpHelper.formatResponse(res, HttpStatus.OK, result);
+    } catch (error) {
+      this.validation.errorHandler(res, error);
     }
+  }
 
-    @UseGuards(JwtGuard, RoleGuard)
-    @Roles(TypeRoleUser.SUPERADMIN)
-    @Delete(':id')
-    async deleteParticipant(
-        @Param('id') id: string,
-        @Res() res: Response,
-    ) {
-        try {
-            await this.participantService.deleteParticipant(id);
-            return this.httpHelper.formatResponse(res, HttpStatus.OK, null);
-        } catch (error) {
-            this.validation.errorHandler(res, error);
-        }
+  @Post()
+  async createParticipant(
+    @Body() dto: CreateParticipantDto,
+    @Res() res: Response,
+  ) {
+    try {
+      const result = await this.participantService.createParticipant(dto);
+      return this.httpHelper.formatResponse(res, HttpStatus.CREATED, result);
+    } catch (error) {
+      this.validation.errorHandler(res, error);
     }
+  }
+
+  @UseGuards(JwtGuard, RoleGuard)
+  @Roles(TypeRoleUser.SUPERADMIN)
+  @Delete(':id')
+  async deleteParticipant(@Param('id') id: string, @Res() res: Response) {
+    try {
+      await this.participantService.deleteParticipant(id);
+      return this.httpHelper.formatResponse(res, HttpStatus.OK, null);
+    } catch (error) {
+      this.validation.errorHandler(res, error);
+    }
+  }
 }
